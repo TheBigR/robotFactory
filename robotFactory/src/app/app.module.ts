f5285e2297e2d9c99982495b166b58b6aa655b36
Roy Nevo <36882185+TheBigR@users.noreply.github.com>
2018-05-07 11:37:46 +0300

    middle of 2nd service

--- robotFactory/src/app/app.module.ts
+++ robotFactory/src/app/app.module.ts
@@ -7,19 +7,22 @@ import { AppComponent } from './app.component';
 import { RobotsComponent } from './robots/robots.component';
 import { RobotDetailComponent } from './robot-detail/robot-detail.component';
 import {RobotService} from "./robot.service";
+import { MessagesComponent } from './messages/messages.component';
+import {MessageService} from "./message.service";
 
 
 @NgModule({
   declarations: [
     AppComponent,
     RobotsComponent,
-    RobotDetailComponent
+    RobotDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
-  providers: [RobotService],
+  providers: [RobotService, MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
