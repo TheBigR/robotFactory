063d8c71014060df97b0c4aab06c13bac4b1171f
thebigr <nevoroy@gmail.com>
2018-05-09 11:51:16 +0300

    finished step 6 - services

--- robotFactory/src/app/message.service.ts
+++ robotFactory/src/app/message.service.ts
@@ -3,6 +3,16 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class MessageService {
 
+  messages: string[] = [];
+
+  add(message: string){
+    this.messages.push(message);
+  }
+
+  clear(){
+    this.messages = [];
+  }
+
   constructor() { }
 
 }
--- robotFactory/src/app/robot.service.ts
+++ robotFactory/src/app/robot.service.ts
@@ -3,13 +3,15 @@ import {Robot} from "./robot";
 import {ROBOTS} from "./mock-robots";
 import {of} from "rxjs/observable/of";
 import {Observable} from "rxjs/Observable";
+import {MessageService} from "./message.service";
 
 @Injectable()
 export class RobotService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   getRobots(): Observable<Robot[]>{
+    this.messageService.add('RobotService: fetched robots');
     return of (ROBOTS);
   }
 
